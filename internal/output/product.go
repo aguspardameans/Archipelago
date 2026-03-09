31f1e752349552725e86843f865b3de83c1a2c90
aguspardameans <116582545+aguspardameans@users.noreply.github.com>
2026-03-09 23:40:50 +0700

    Create product.go

---
+++ internal/output/product.go
@@ -0,0 +1,302 @@
+[
+  {
+    "category": "Room",
+    "name": "Standard Room 7",
+    "revenue": "MjMyMDA2OC4xOQ==",
+    "units_sold": 8229
+  },
+  {
+    "category": "Room",
+    "name": "Suite Room 23",
+    "revenue": "ODQ2NDIuNTE=",
+    "units_sold": 292
+  },
+  {
+    "category": "Room",
+    "name": "Standard Room 1",
+    "revenue": "Nzg0MjEuODU=",
+    "units_sold": 278
+  },
+  {
+    "category": "Dining",
+    "name": "Dining Package 45",
+    "revenue": "NzgyMTcuMTc=",
+    "units_sold": 276
+  },
+  {
+    "category": "Spa",
+    "name": "Spa Package 33",
+    "revenue": "NzgxODMuNjE=",
+    "units_sold": 276
+  },
+  {
+    "category": "Room",
+    "name": "Deluxe Room 16",
+    "revenue": "NzY4MDkuNDI=",
+    "units_sold": 267
+  },
+  {
+    "category": "Dining",
+    "name": "Dining Package 48",
+    "revenue": "NzU5OTkuODA=",
+    "units_sold": 266
+  },
+  {
+    "category": "Room",
+    "name": "Suite Room 25",
+    "revenue": "NzQ0MTQuMjE=",
+    "units_sold": 287
+  },
+  {
+    "category": "Dining",
+    "name": "Dining Package 42",
+    "revenue": "NzMyNzIuOTY=",
+    "units_sold": 244
+  },
+  {
+    "category": "Room",
+    "name": "Standard Room 8",
+    "revenue": "NzI3MDMuNjI=",
+    "units_sold": 252
+  },
+  {
+    "category": "Spa",
+    "name": "Spa Package 32",
+    "revenue": "NzAzNzcuMzc=",
+    "units_sold": 250
+  },
+  {
+    "category": "Room",
+    "name": "Deluxe Room 17",
+    "revenue": "NzAzMjAuNTE=",
+    "units_sold": 268
+  },
+  {
+    "category": "Room",
+    "name": "Suite Room 29",
+    "revenue": "Njk4MDEuMzc=",
+    "units_sold": 250
+  },
+  {
+    "category": "Room",
+    "name": "Deluxe Room 15",
+    "revenue": "Njk2MzEuMjk=",
+    "units_sold": 241
+  },
+  {
+    "category": "Spa",
+    "name": "Spa Package 40",
+    "revenue": "Njk0NDYuMDU=",
+    "units_sold": 249
+  },
+  {
+    "category": "Room",
+    "name": "Suite Room 24",
+    "revenue": "NjkyNjcuMDc=",
+    "units_sold": 239
+  },
+  {
+    "category": "Dining",
+    "name": "Dining Package 46",
+    "revenue": "Njg5NDcuNjg=",
+    "units_sold": 261
+  },
+  {
+    "category": "Room",
+    "name": "Standard Room 6",
+    "revenue": "Njg4NDQuNDQ=",
+    "units_sold": 244
+  },
+  {
+    "category": "Room",
+    "name": "Deluxe Room 20",
+    "revenue": "NjgzNDguMTk=",
+    "units_sold": 251
+  },
+  {
+    "category": "Dining",
+    "name": "Dining Package 43",
+    "revenue": "NjgzMjcuMzk=",
+    "units_sold": 253
+  },
+  {
+    "category": "Room",
+    "name": "Standard Room 4",
+    "revenue": "NjgwODEuMzg=",
+    "units_sold": 246
+  },
+  {
+    "category": "Room",
+    "name": "Standard Room 5",
+    "revenue": "Njc5NTEuMzM=",
+    "units_sold": 254
+  },
+  {
+    "category": "Dining",
+    "name": "Dining Package 44",
+    "revenue": "Njc4NjAuNDc=",
+    "units_sold": 261
+  },
+  {
+    "category": "Room",
+    "name": "Suite Room 26",
+    "revenue": "Njc3MzQuNzI=",
+    "units_sold": 265
+  },
+  {
+    "category": "Room",
+    "name": "Suite Room 22",
+    "revenue": "NjY3MTMuMDE=",
+    "units_sold": 253
+  },
+  {
+    "category": "Room",
+    "name": "Suite Room 27",
+    "revenue": "NjYzMjIuMTU=",
+    "units_sold": 247
+  },
+  {
+    "category": "Room",
+    "name": "Standard Room 10",
+    "revenue": "NjYxODQuMTM=",
+    "units_sold": 250
+  },
+  {
+    "category": "Room",
+    "name": "Deluxe Room 11",
+    "revenue": "NjYwMTkuMzg=",
+    "units_sold": 255
+  },
+  {
+    "category": "Dining",
+    "name": "Dining Package 49",
+    "revenue": "NjU2MDYuODE=",
+    "units_sold": 234
+  },
+  {
+    "category": "Spa",
+    "name": "Spa Package 35",
+    "revenue": "NjU0OTYuNTM=",
+    "units_sold": 237
+  },
+  {
+    "category": "Dining",
+    "name": "Dining Package 41",
+    "revenue": "NjUwODcuMzM=",
+    "units_sold": 257
+  },
+  {
+    "category": "Dining",
+    "name": "Dining Package 50",
+    "revenue": "NjQ3MDYuOTU=",
+    "units_sold": 241
+  },
+  {
+    "category": "Spa",
+    "name": "Spa Package 37",
+    "revenue": "NjQ1NjkuMDE=",
+    "units_sold": 236
+  },
+  {
+    "category": "Room",
+    "name": "Deluxe Room 12",
+    "revenue": "NjM4OTIuMjM=",
+    "units_sold": 232
+  },
+  {
+    "category": "Room",
+    "name": "Suite Room 28",
+    "revenue": "NjM4MjUuMjQ=",
+    "units_sold": 223
+  },
+  {
+    "category": "Room",
+    "name": "Standard Room 2",
+    "revenue": "NjI1NDIuMjM=",
+    "units_sold": 230
+  },
+  {
+    "category": "Room",
+    "name": "Standard Room 3",
+    "revenue": "NjIyODguNTg=",
+    "units_sold": 239
+  },
+  {
+    "category": "Room",
+    "name": "Deluxe Room 19",
+    "revenue": "NjEyODEuNzA=",
+    "units_sold": 222
+  },
+  {
+    "category": "Dining",
+    "name": "Dining Package 47",
+    "revenue": "NjEyMjcuMDI=",
+    "units_sold": 215
+  },
+  {
+    "category": "Spa",
+    "name": "Spa Package 36",
+    "revenue": "NjEwNzEuMzc=",
+    "units_sold": 222
+  },
+  {
+    "category": "Room",
+    "name": "Suite Room 30",
+    "revenue": "NjA4OTMuNjY=",
+    "units_sold": 230
+  },
+  {
+    "category": "Spa",
+    "name": "Spa Package 31",
+    "revenue": "NjA2NDQuNTQ=",
+    "units_sold": 243
+  },
+  {
+    "category": "Spa",
+    "name": "Spa Package 34",
+    "revenue": "NjAyODAuMTk=",
+    "units_sold": 236
+  },
+  {
+    "category": "Room",
+    "name": "Standard Room 9",
+    "revenue": "NTk5MTkuNzE=",
+    "units_sold": 225
+  },
+  {
+    "category": "Spa",
+    "name": "Spa Package 39",
+    "revenue": "NTkyOTcuNTM=",
+    "units_sold": 217
+  },
+  {
+    "category": "Spa",
+    "name": "Spa Package 38",
+    "revenue": "NTgzNjcuNDA=",
+    "units_sold": 217
+  },
+  {
+    "category": "Room",
+    "name": "Deluxe Room 18",
+    "revenue": "NTgxMTYuNjY=",
+    "units_sold": 213
+  },
+  {
+    "category": "Room",
+    "name": "Deluxe Room 14",
+    "revenue": "NTU0NDEuMTU=",
+    "units_sold": 217
+  },
+  {
+    "category": "Room",
+    "name": "Deluxe Room 13",
+    "revenue": "NTQyNDEuNjA=",
+    "units_sold": 205
+  },
+  {
+    "category": "Room",
+    "name": "Suite Room 21",
+    "revenue": "NDc2ODAuNTg=",
+    "units_sold": 183
+  }
+]
